cbe5373428ccfc98c2353d46b0885a77131e43d9
Liz Ainslie <lizainslie16@gmail.com>
2022-10-23 06:48:44 -0700

    chore(init): begin initialize libGDX project

---
+++ core/src/dev/lizainslie/silence/BloodySilenceGame.kt
@@ -0,0 +1,27 @@
+package dev.lizainslie.silence
+
+import com.badlogic.gdx.ApplicationAdapter
+import com.badlogic.gdx.graphics.Texture
+import com.badlogic.gdx.graphics.g2d.SpriteBatch
+import com.badlogic.gdx.utils.ScreenUtils
+
+class BloodySilenceGame : ApplicationAdapter() {
+    private var batch: SpriteBatch? = null
+    private var img: Texture? = null
+    override fun create() {
+        batch = SpriteBatch()
+        img = Texture("badlogic.jpg")
+    }
+
+    override fun render() {
+        ScreenUtils.clear(1f, 0f, 0f, 1f)
+        batch!!.begin()
+        batch!!.draw(img, 0f, 0f)
+        batch!!.end()
+    }
+
+    override fun dispose() {
+        batch!!.dispose()
+        img!!.dispose()
+    }
+}
---
+++ desktop/src/dev/lizainslie/silence/DesktopLauncher.kt
@@ -0,0 +1,15 @@
+package dev.lizainslie.silence
+
+import com.badlogic.gdx.backends.lwjgl3.Lwjgl3Application
+import com.badlogic.gdx.backends.lwjgl3.Lwjgl3ApplicationConfiguration
+
+// Please note that on macOS your application needs to be started with the -XstartOnFirstThread JVM argument
+object DesktopLauncher {
+    @JvmStatic
+    fun main(arg: Array<String>) {
+        val config = Lwjgl3ApplicationConfiguration()
+        config.setForegroundFPS(60)
+        config.setTitle("Bloody Silence")
+        Lwjgl3Application(BloodySilenceGame(), config)
+    }
+}
