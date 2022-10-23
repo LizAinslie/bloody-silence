4ca03daa1e7e513edb43ac6e29933998314ddb25
Liz Ainslie <lizainslie16@gmail.com>
2022-10-23 11:02:22 -0700

    chore(game): continue fleshing out prop logic

--- core/src/dev/lizainslie/silence/prop/Prop.kt
+++ core/src/dev/lizainslie/silence/prop/Prop.kt
@@ -1,5 +1,9 @@
 package dev.lizainslie.silence.prop
 
+import dev.lizainslie.silence.player.Player
+
 interface Prop {
     val descriptor: String
+
+    fun onInteract(player: Player)
 }
--- core/src/dev/lizainslie/silence/prop/PropDescriptors.kt
+++ core/src/dev/lizainslie/silence/prop/PropDescriptors.kt
@@ -1,12 +1,13 @@
 package dev.lizainslie.silence.prop
 
 /**
- * Prop descriptors are baked into map models and allow the game to determine
- * prop placement inside the map.
+ * Prop descriptors are baked into map and object models and allow the game to
+ * determine prop placement inside the map and differentiate objects.
  *
  * @author Liz Ainslie
  */
 object PropDescriptors {
     const val BLUEPRINT = "prop_blueprint"
     const val CRATE = "prop_crate"
+    const val LOOTBOX = "prop_lootbox"
 }
